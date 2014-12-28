3bc3c8674df31d2c2a185dfa0aa04aa373c76f11
ProjectInfinity <a.sjur91@gmail.com>
2014-12-28 04:09:03 +0100

    Push this so that I can work on another computer.

---
+++ src/ProjectInfinity/PlayerFlow/PlayerFlow.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ProjectInfinity\PlayerFlow;
+
+use pocketmine\plugin\PluginBase;
+
+class PlayerFlow extends PluginBase {
+
+    public $interval;
+
+    public function onEnable() {
+
+    }
+
+    public function onDisable() { /** Anything static etc has to be nulled here. **/ }
+
+}
